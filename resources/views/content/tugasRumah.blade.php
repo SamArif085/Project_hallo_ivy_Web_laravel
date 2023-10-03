1407666264db9f242f07ffacd14e9a71d6e186d3
SamArif <samarif085@gmail.com>
2023-10-03 22:39:27 +0700

    update
    modal tugas rumah

--- resources/views/content/tugasRumah.blade.php
+++ resources/views/content/tugasRumah.blade.php
@@ -20,6 +20,12 @@
                         <div class="card-body">
                             <h5 class="card-title">{{ $cardTitle }}</h5>
 
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-success" data-bs-toggle="modal"
+                                data-bs-target="#addPR">
+                                Tambah
+                            </button>
+
                             <!-- Table with stripped rows -->
                             <table class="table datatable">
                                 <thead>
@@ -113,125 +119,6 @@
             </div>
         </section>
 
-    </main><!-- End #main -->
-    {{--
-    <!-- Kumpulan Modal -->
-    <!-- Modal Tambah Materi -->
-    <div class="modal fade" id="addMateri" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-md">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">{{ $modalTitle['materi'] }}</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ route('tambahMateri') }}" method="post">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="exampleFormControlInput1" class="form-label">Pilih Kelas</label>
-                            <select class="form-select" id="inputGroupSelect01" name="pilKelas" required>
-                                <option selected disabled value="">Pilih Kelas...</option>
-                                <option value="1">Kelas Satu</option>
-                                <option value="2">Kelas Dua</option>
-                                <option value="3">DST</option>
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleFormControlInput1" class="form-label">Pilih Materi Yang Akan Di
-                                Upload</label>
-                            <div>
-                                <button type="button" class="btn btn-warning" onclick="kata()"><i
-                                        class="bi bi-fonts"></i></button>
-                                <button type="button" class="btn btn-secondary" onclick="gambar()"><i
-                                        class="bi bi-image"></i></button>
-                                <button type="button" class="btn btn-primary" onclick="video()"><i
-                                        class="bi bi-play-btn"></i></button>
-                            </div>
-                        </div>
-                        <div id="formText">
-                            <!-- Form dinamis akan ditambahkan di sini -->
-                        </div>
-                        <div id="formGambar">
-                            <!-- Form dinamis akan ditambahkan di sini -->
-                        </div>
-                        <div id="formVideo">
-                            <!-- Form dinamis akan ditambahkan di sini -->
-                        </div>
-                </div>
-                <div class="modal-footer">
-                    <!--<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>-->
-                    <button type="submit" class="btn btn-success">Simpan Data</button>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div> --}}
+    </main>
+    @include('content.modal.modalTugasRumah');
 @endsection
-{{-- @section('script')
-    <script>
-        // Function Form Text
-        function kata() {
-            let data =
-                `<label for="exampleFormControlInput1" class="form-label">Deskripsi</label>
-                        <div class="row">
-                            <div class="col-10">
-                                <input type="text" class="form-control mb-3" name="text" id="text" required>
-                            </div>
-                            <div class="col-2">
-                                <button type="button" class="btn btn-danger" onclick="hapus()"><i
-                                        class="bi bi-trash"></i></button>
-                            </div>
-                        </div>`;
-            // console.log(data);
-            $('#formText').html(data)
-        }
-        // Function Hapus Form Text
-        function hapus() {
-            $('#formText').html('')
-        }
-
-        // Fucntion Form Gambar
-        function gambar() {
-            let data =
-                `<label for="exampleFormControlInput1" class="form-label">Upload Gambar</label>
-                        <div class="row">
-                            <div class="col-10">
-                                <input type="file" class="form-control mb-3" name="text" id="text" required>
-                            </div>
-                            <div class="col-2">
-                                <button type="button" class="btn btn-danger" onclick="hapusGam()"><i
-                                        class="bi bi-trash"></i></button>
-                            </div>
-                        </div>`;
-            // console.log(data);
-            $('#formGambar').html(data)
-        }
-
-        // Function Hapus Form Gambar
-        function hapusGam() {
-            $('#formGambar').html('')
-        }
-
-        // Function Form Video
-        function video() {
-            let data =
-                `<label for="exampleFormControlInput1" class="form-label">Upload Video</label>
-                <div class="row">
-                    <div class="col-10">
-                        <input type="file" class="form-control mb-3" name="text" id="text" required>
-                    </div>
-                    <div class="col-2">
-                        <button type="button" class="btn btn-danger" onclick="hapusVid()"><i
-                                class="bi bi-trash"></i></button>
-                    </div>
-                </div>`;
-            // console.log(data);
-            $('#formVideo').html(data)
-        }
-
-        // Function Hapus Form Video
-        function hapusVid() {
-            $('#formVideo').html('')
-        }
-    </script>
-@endsection --}}
